624478b8fee7f125c9dc8dded23daa8439139ed3
Kai Eugene <kai.yougene@gmail.com>
2024-03-26 17:23:10 +0900

    feat: navbar scroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,36 @@ import React, { useState } from "react";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <nav className="w-full">
       {/* primary */}
       <div className="hidden lg:flex h-[10vh] w-full fixed top-0 left-0 z-10 bg-[rgba(242, 245, 250, 1)] backdrop-blur-[4px] text-black">
-        <div className="w-1/2 h-full flex justify-start items-center ml-52">
+        <div
+          className="w-1/2 h-full flex justify-start items-center ml-52 cursor-pointer"
+          onClick={() => scrollToSection("header")}
+        >
           Logo
         </div>
         <ul className="w-1/2 h-full mr-52 flex justify-end items-center gap-10">
           {["Skills", "Work", "Contact"].map((item, index) => {
             return (
               <li className="hover:text-gray-500" key={index}>
-                <a href={`#${item.toLowerCase()}`}>{item}</a>
+                <a
+                  href={`#${item.toLowerCase()}`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    scrollToSection(item.toLowerCase());
+                  }}
+                >
+                  {item}
+                </a>
               </li>
             );
           })}
@@ -55,9 +73,15 @@ const Navbar = () => {
       >
         <div className="px-8 mb-7 text-center">
           <div className="flex flex-col justify-end gap-8 font-bold tracking-wider ">
-            <a href="#skills">Skills</a>
-            <a href="#projects">Work</a>
-            <a href="#contact">Contact</a>
+            <a href="#skills" onClick={() => scrollToSection("skills")}>
+              Skills
+            </a>
+            <a href="#projects" onClick={() => scrollToSection("projects")}>
+              Work
+            </a>
+            <a href="#contact" onClick={() => scrollToSection("contact")}>
+              Contact
+            </a>
           </div>
         </div>
       </div>
